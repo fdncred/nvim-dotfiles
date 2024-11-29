fd52085c55c921230deb64d7cc1f7ff4d87165f7
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2024-11-29 08:52:50 -0600

    add text objects

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -14,6 +14,31 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
+      -- OPTIONAL!! These enable ts-specific textobjects.
+      -- So you can hit `yaf` to copy the closest function,
+      -- `dif` to clear the contet of the closest function,
+      -- or whatever keys you map to what query.
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              -- For example:
+              -- Nushell only
+              ["aP"] = "@pipeline.outer",
+              ["iP"] = "@pipeline.inner",  
+              -- supported in other languages as well
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["al"] = "@loop.outer",
+              ["il"] = "@loop.inner",
+              ["aC"] = "@conditional.outer",
+              ["iC"] = "@conditional.inner",
+              ["iS"] = "@statement.inner",
+              ["aS"] = "@statement.outer",
+          }, -- keymaps
+        }, -- select
+      }, -- textobjects      
     },
     --config = function(_, opts)
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
