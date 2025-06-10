712897e097a9c45c17389cd63685f97928aff3da
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2025-06-10 16:15:56 -0500

    update nushell integration

--- lua/options.lua
+++ lua/options.lua
@@ -61,9 +61,11 @@ vim.opt.scrolloff = 10
 
 -- for nushell piping
 vim.opt.shell = 'nu'
-vim.opt.shellcmdflag = '--stdin --no-newline -c '
+vim.opt.shellcmdflag = '--login --stdin --no-newline -c'
+vim.opt.shellredir = 'out+err> %s',
 vim.opt.shellquote = ''
 vim.opt.shellxquote = ''
+vim.opt.shellxescape = ''
 vim.opt.shelltemp = false
-vim.opt.shellpipe = ' | tee { save -f --raw %s}'
+vim.opt.shellpipe = "| complete | update stderr { ansi strip } | tee { get stderr | save --force --raw %s } | into record"
 -- vim: ts=2 sts=2 sw=2 et
