9865af9a2c2df933c75ed37ed9c3952d2bb1da6d
Darren Schroeder <343840+fdncred@users.noreply.github.com>
2026-05-15 09:08:45 -0500

    enable custom UI settings for improved message display

--- init.lua
+++ init.lua
@@ -97,6 +97,17 @@ vim.g.loaded_netrwPlugin = 1
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+require('vim._core.ui2').enable({
+  enable = true,
+  msg = {
+    target = 'cmd',
+    pager = { height = 0.5 },
+    dialog = { height = 0.5 },
+    cmd = { height = 0.5 },
+    msg = { height = 0.5, timeout = 4500 },
+  },
+})
+
 -- [[ Setting options ]]
 require 'options'
 
